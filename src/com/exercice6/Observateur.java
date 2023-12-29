ad5a9779c84a81f91cb9d84bc1d8ac84762be880
DIMOKRATI Houssam <houssamdimokrati@gmail.com>
2023-12-29 19:17:53 +0100

    Implementation d'exercice6

---
+++ src/com/exercice6/Observateur.java
@@ -0,0 +1,6 @@
+package com.exercice6;
+
+public interface Observateur
+{
+    void actualise();
+}
---
+++ src/com/exercice6/Sujet.java
@@ -0,0 +1,21 @@
+package com.exercice6;
+
+import java.util.*;
+public abstract class Sujet
+{
+    protected List<Observateur> observateurs =
+            new ArrayList<Observateur>();
+    public void ajoute(Observateur observateur)
+    {
+        observateurs.add(observateur);
+    }
+    public void retire(Observateur observateur)
+    {
+        observateurs.remove(observateur);
+    }
+    public void notifie()
+    {
+        for (Observateur observateur: observateurs)
+            observateur.actualise();
+    }
+}
